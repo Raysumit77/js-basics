69f7fb641b2cffb89064bcc88f7afe13d725dd1e
Sumit Yadav <ahsiray79@gmail.com>
2024-01-23 18:58:30 +0545

    variable let and console

---
+++ index.js
@@ -0,0 +1,32 @@
+//print the message 
+console.log("Hello World");
+//variables
+//var (ES5)
+//var,let,const
+var braodwaystudent="Sumit";
+braodwaystudent = "Biky";
+//always use camlecase for joining two or more words for variables
+console.log(braodwaystudent);
+//let,const (ES6)
+let student = "sumit"
+student = "sumit"
+console.log(student);
+const student = "sumit"
+console.log(student);
+//writing comment for many
+/*create 4 variable using const 
+keyword
+ for table 
+ ,fan,
+ chair
+ ,pen
+ */
+const table ="dinning table";
+const blackpen ="paras"
+const chair = "furniture"
+const pen = "cello"
+  
+
+console.log (table,blackpen,chair,pen );
+console.log(2 + 2);
+//use var and let keyword for 4 variable
